c1ad1f2c83bb978fefb320541e35a6884bee53c5
chen-jason-chen <chen_jason_chen@yahoo.com>
2017-01-21 00:36:11 -0500

    Initiate ionic project

---
+++ whatshappening/www/js/app.js
@@ -0,0 +1,41 @@
+var app = angular.module('whatshappening', [
+  'ionic',
+  'ngMap',
+  'ngCordova',
+  'swetappdemo.controllers',
+  'swetappdemo.services'
+]);
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+});
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+
+  // Main page
+  .state('main', {
+    url: '/main',
+    templateUrl: 'templates/main/main.html',
+    controller: 'MainCtrl'
+  })
+
+  ;
+
+  // Default route
+  $urlRouterProvider.otherwise('/main');
+
+});
---
+++ whatshappening/www/js/controllers.js
@@ -0,0 +1,5 @@
+var app = angular.module('swetappdemo.controllers', []);
+
+app.controller('MainCtrl', function ($scope) {
+  console.log("Hello World!");
+});
