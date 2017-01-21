c9ecd3df559bfa331891dd6fef3398bde05519c6
chen-jason-chen <chen_jason_chen@yahoo.com>
2017-01-21 13:49:32 -0500

    Add list functionality

--- whatshappening/www/js/controllers.js
+++ whatshappening/www/js/controllers.js
@@ -1,16 +1,32 @@
 var app = angular.module('whatshappening.controllers', []);
 
+
 app.controller('MainCtrl', function ($scope) {
 
 });
 
-app.controller('ListCtrl', function ($scope) {
 
+app.controller('ListCtrl', function ($scope, $ionicLoading, ListService) {
+  console.log("Loading ListCtrl");
+  $ionicLoading.show({template:'Loading feed...'});
+
+  $scope.content = ListService;
+  $scope.content.loadEvents().then(function() {
+    $ionicLoading.hide();
+  });
+
+  $scope.doRefresh = function() {
+    $scope.content.loadEvents().then(function () {
+      $scope.$broadcast('scroll.refreshComplete');
+    });
+  };
 });
+
 
 app.controller('DetailsCtrl', function ($scope) {
 
 });
+
 
 app.controller('EventCtrl', function ($scope) {
   $scope.formData = {
@@ -24,9 +40,11 @@ app.controller('EventCtrl', function ($scope) {
   };
 
   $scope.create = function (form) {
-    console.log("LoginCtrl::login");
+    console.log("EventCtrl create event called");
     if (form.$valid) {
-      // call backend function
+      // TODO: call Firebase add event function
+      console.log(form.start);
+      console.log(form.end);
     } else {
       console.log("Invalid form");
     }
--- whatshappening/www/js/services.js
+++ whatshappening/www/js/services.js
@@ -1,2 +1,29 @@
 var app = angular.module('whatshappening.services', []);
 
+app.factory('ListService', function ($q) {
+
+  var self = {
+    // List of events extracted by Firebase
+    'events': [
+      // Temp list of events for testing purposes
+    ]
+  };
+
+  self.loadEvents = function() {
+    // TODO: Firebase extract array of events
+
+    // Temp implementation for testing
+    var defer = $q.defer();
+
+    var event1 = {name:"badminton", location:"Athletic Centre", address:"55 Harbord St., Toronto",
+                  description:"Play some badminton"};
+    var event2 = {name:"chess", location:"Hart House", address:"7 Hart House Cir., Toronto",
+      description:"Play some chess"};
+    self.events.push(event1, event2);
+    defer.resolve(self.events);
+
+    return defer.promise;
+  };
+
+  return self;
+});
