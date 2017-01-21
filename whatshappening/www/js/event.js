9f11814cb6042d62e27e0d5b8220819c9e4a1c3e
christinekc <Chrsitine@Christines-MacBook-Pro.local>
2017-01-21 11:42:52 -0500

    create event in database

---
+++ whatshappening/www/js/event.js
@@ -0,0 +1,24 @@
+// Create an event
+var name = document.getElementById("name");
+var venue = document.getElementById("venue");
+var address = document.getElementById("address");
+var city = document.getElementById("city");
+var province = document.getElementById("province");
+var postalCode = document.getElementById("postalCode");
+var description = document.getElementById("description");
+// TODO: might have to separate elements for time
+var startTime = document.getElementById("startTime");
+var endTime = document.getElementById("endTime");
+var price = document.getElementById("price");
+
+var ageRestriction = document.getElementById("ageRestriction");
+var password = document.getElementById("password");
+// TODO: where to verify password retype?
+
+var submitBtn = document.getElementById("submitBtn");
+
+function submitClick() {
+
+  window.alert("HI");
+
+}
