bbb1b418b262bb456b04ffcf9644cfb34c527ee1
chen-jason-chen <chen_jason_chen@yahoo.com>
2017-01-21 01:37:21 -0500

    Add app config

--- whatshappening/www/js/app.js
+++ whatshappening/www/js/app.js
@@ -8,12 +8,10 @@ var app = angular.module('whatshappening', [
 
 app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
+    // Hide the accessory bar by default
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
@@ -26,12 +24,36 @@ app.config(function ($stateProvider, $urlRouterProvider) {
 
   $stateProvider
 
-  // Main page
-  .state('main', {
-    url: '/main',
-    templateUrl: 'templates/main/main.html',
-    controller: 'MainCtrl'
-  })
+    // Main page
+    .state('main', {
+      url: '/main',
+      templateUrl: 'templates/main.html',
+      controller: 'MainCtrl'
+    })
+
+    .state('list', {
+      url: '/list',
+      templateUrl: 'templates/list.html',
+      controller: 'ListCtrl'
+    })
+
+    .state('details', {
+      url: '/details',
+      templateUrl: 'templates/details.html',
+      controller: 'DetailsCtrl'
+    })
+
+    .state('map', {
+      url: '/map',
+      templateUrl: 'templates/map.html',
+      controller: 'MapCtrl'
+    })
+
+    .state('event', {
+      url: '/event',
+      templateUrl: 'templates/event.html',
+      controller: 'EventCtrl'
+    })
 
   ;
 
